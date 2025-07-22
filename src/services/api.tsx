04b24f953dbdef8b45a9acc863da43292c03a446
wimbodwi26 <wimbodwin@gmail.com>
2025-07-22 21:28:53 +0700

    Update api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'https://service-crud-api-production.up.railway.app/'
+    baseURL: 'https://service-crud-api-production.up.railway.app/api'
 })
 
 export default Api
